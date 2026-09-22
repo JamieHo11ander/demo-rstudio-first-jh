f0c4b2a8c868edaf90eb003ba5e27ca277677b05
JamieHo11ander <jwh173@gmail.com>
2026-09-22 10:00:17 -0700

    edit to readme and added test-analysis script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # demo-rstudio-first-jh
 
 This is a demo repo that I created in rstudio first, then sent to github
+
+I am making an edit on this file  - the rstudio first one
---
+++ test-analysis.R
@@ -0,0 +1,8 @@
+# view the first few rows of mtcars dataset
+head(mtcars)
+
+#calc the avg mmp among all cars, save as variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print the mean
+mean_mpg
